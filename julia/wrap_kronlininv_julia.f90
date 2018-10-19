d98c440f564953ef6f7ab8ddc03149c42d0f62cc
Andrea Zunino <inverseproblem@users.noreply.github.com>
2018-10-19 11:03:04 +0200

    fixed typos

--- julia/wrap_kronlininv_julia.f90
+++ julia/wrap_kronlininv_julia.f90
@@ -2,16 +2,16 @@
 
 !------------------------------------------------------------------------
 !
-!    Copyright 2018  Andrea Zunino 
+!    Copyright 2018, Andrea Zunino 
 !
-!    This file is part of  KronLinInv.
+!    This file is part of KronLinInv.
 !
 !    KronLinInv is free software: you can redistribute it and/or modify
 !    it under the terms of the GNU General Public License as published by
 !    the Free Software Foundation, either version 3 of the License, or
 !    (at your option) any later version.
 !
-!    Phemgp is distributed in the hope that it will be useful,
+!    KronLinInv is distributed in the hope that it will be useful,
 !    but WITHOUT ANY WARRANTY; without even the implied warranty of
 !    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 !    GNU General Public License for more details.
